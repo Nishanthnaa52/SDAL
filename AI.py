1bd5949a12115e279a31f47c99d425af9467d9e3
Nishanth <98414644+Nishanthnaa52@users.noreply.github.com>
2023-10-19 23:09:34 +0530

    AI.py

---
+++ AI.py
@@ -0,0 +1,29 @@
+import pyttsx3
+
+engine = pyttsx3.init("espeak")
+voices=engine.getProperty('voices')
+engine.setProperty('voice',voices[17].id)
+print(voices[17].id)
+
+def speak(audio): 
+    engine.getProperty('rate')
+    engine.setProperty('rate',150)
+    engine.say(audio)
+    engine.runAndWait()
+
+if __name__ == "__main__":
+    speak("Hello nishanth, how are you!")import pyttsx3
+
+engine = pyttsx3.init("espeak")
+voices=engine.getProperty('voices')
+engine.setProperty('voice',voices[17].id)
+print(voices[17].id)
+
+def speak(audio): 
+    engine.getProperty('rate')
+    engine.setProperty('rate',150)
+    engine.say(audio)
+    engine.runAndWait()
+
+if __name__ == "__main__":
+    speak("Hello nishanth, how are you!")
