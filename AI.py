3d3dae49a45b0d0488bd5a8d7b5e1cfd1c018073
Nishanth <nishanth@pop-os.localdomain>
2023-10-21 21:57:13 +0530

    Step-1

--- AI.py
+++ AI.py
@@ -1,15 +1,50 @@
-import pyttsx3
+import os
+import speech_recognition as sp
+import sounddevice
+import datetime
+import random
 
-engine = pyttsx3.init("espeak")
-voices=engine.getProperty('voices')
-engine.setProperty('voice',voices[17].id)
-print(voices[17].id)
+os.system("clear")
 
-def speak(audio): 
-    engine.getProperty('rate')
-    engine.setProperty('rate',150)
-    engine.say(audio)
-    engine.runAndWait()
+def speak(audio):
+    print("AI : ",audio)
+    os.system('''echo '%s' | festival --tts'''%(audio))
+    
+
+def r_voice():
+    r=sp.Recognizer()
+    with sp.Microphone() as mp:
+        print("Recogning...")
+        r.pause_threshold=1
+        audio=r.listen(mp)
+    
+    try:
+        print("wait...")
+        query=r.recognize_google(audio,language="en-in")
+        print("user side : ",query)
+
+        return query
+    except Exception as e:
+        print(e)
+        speak("Say agin place...")
+
+def welcome():
+    hour = int(datetime.datetime.now().hour)
+    print(hour)
+    if(hour >= 1 and hour < 12):
+        speak("Good morning sir...")
+    elif(hour >= 12 and hour < 4):
+        speak("Good afternoon sir...")
+    else:
+        speak("Good evening sir...")
+    speak("How can i help you sir...")
 
 if __name__ == "__main__":
-    speak("Hello nishanth, how are you!")
+    welcome()
+    #ans = r_voice()
+    #speak(ans)
+    # welcome = "hello how are you! what is your name?"
+    # speak(welcome)
+    # q = input("enter your name :")
+    # name = "hi " + q +"how can i help you."
+    # speak(name)
