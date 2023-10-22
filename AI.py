eaa2905326fe05a5f4a85f1f8ee6ffe32b9422f0
Nishanth <nishanth@pop-os.localdomain>
2023-10-22 15:47:14 +0530

    step-2

--- AI.py
+++ AI.py
@@ -2,35 +2,36 @@ import os
 import speech_recognition as sp
 import sounddevice
 import datetime
-import random
+import wikipedia
+
 
 os.system("clear")
 
 def speak(audio):
     print("AI : ",audio)
-    os.system('''echo '%s' | festival --tts'''%(audio))
+    os.system("echo '''%s''' | festival --tts"%(audio))
     
 
 def r_voice():
     r=sp.Recognizer()
     with sp.Microphone() as mp:
         print("Recogning...")
-        r.pause_threshold=1
+        r.pause_threshold=0.8
         audio=r.listen(mp)
     
     try:
         print("wait...")
         query=r.recognize_google(audio,language="en-in")
         print("user side : ",query)
-
+        
         return query
+
     except Exception as e:
         print(e)
         speak("Say agin place...")
-
+        
 def welcome():
     hour = int(datetime.datetime.now().hour)
-    print(hour)
     if(hour >= 1 and hour < 12):
         speak("Good morning sir...")
     elif(hour >= 12 and hour < 4):
@@ -40,11 +41,17 @@ def welcome():
     speak("How can i help you sir...")
 
 if __name__ == "__main__":
-    welcome()
-    #ans = r_voice()
-    #speak(ans)
-    # welcome = "hello how are you! what is your name?"
-    # speak(welcome)
-    # q = input("enter your name :")
-    # name = "hi " + q +"how can i help you."
-    # speak(name)
+    #welcome()
+    while True:
+        query=r_voice().lower()
+
+        if "goodbye" in query:
+            os.system("exit")
+
+        if "wikipedia" in query:
+            speak("searching in wikipedia")
+            query=query.replace("wikipedia","")
+            results=wikipedia.summary(query,sentences=2)
+            speak("According to wikipedia")
+            speak(results)
+            
