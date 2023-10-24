0b18132dd99033e7db5dd6985b176b9469e24fac
Nishanth <nishanth@pop-os.localdomain>
2023-10-24 11:23:11 +0530

    step-3

--- AI.py
+++ AI.py
@@ -7,9 +7,17 @@ import wikipedia
 
 os.system("clear")
 
+
+def file_read(audio):
+    file = open('read.txt', 'w') 
+    file.write(audio) 
+    file.close()
+
 def speak(audio):
     print("AI : ",audio)
-    os.system("echo '''%s''' | festival --tts"%(audio))
+    file_read(audio)
+    os.system("cat read.txt | festival --tts")
+    os.remove("read.txt")
     
 
 def r_voice():
@@ -18,14 +26,15 @@ def r_voice():
         print("Recogning...")
         r.pause_threshold=0.8
         audio=r.listen(mp)
-    
+        
+        
     try:
         print("wait...")
         query=r.recognize_google(audio,language="en-in")
         print("user side : ",query)
-        
-        return query
+        file_read(query)
 
+        return query
     except Exception as e:
         print(e)
         speak("Say agin place...")
@@ -41,17 +50,19 @@ def welcome():
     speak("How can i help you sir...")
 
 if __name__ == "__main__":
-    #welcome()
-    while True:
+    welcome()
+    condition = True
+    while condition:
         query=r_voice().lower()
 
         if "goodbye" in query:
-            os.system("exit")
+            speak("Good bye sir...")
+            condition = False
 
         if "wikipedia" in query:
             speak("searching in wikipedia")
             query=query.replace("wikipedia","")
-            results=wikipedia.summary(query,sentences=2)
+            results=wikipedia.summary(query,sentences=3)
             speak("According to wikipedia")
             speak(results)
             
